3d389b4a74009d36b0fd840259c4d89ac941c8c1
Matheus Teles <matheus07gitteles@gmail.com>
2025-07-29 16:55:44 -0300

    tipos de dados

--- 1. Introdução/1.1 console.js
+++ 1. Introdução/1.1 console.js
@@ -1,4 +1,8 @@
 //-----CONSOLE-----
-console.log("Print")
-console.log("Hello world")
-console.log("Teste")
+
+console.log("Print");
+console.log("Hello world");
+console.log("Senai");
+alert("Mensagem de alerta");
+console.error("Mensagem de erro");
+console.warn("Mensagem de aviso");
---
+++ 2. Tipos de dados/2.1 Tipagem.js
@@ -0,0 +1,11 @@
+// ------TIPAGEM----------
+// Tipagem é a forma como os dados são classificados em uma linguagem
+
+console.log("Tipos de dados:")
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof "Matheus")
+console.log(typeof true)
+
+console.log(2 + 2)
+console.log("2" + 2)
